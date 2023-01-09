f4f16bd4639dc0c77ee51afae6acce4aa9cfa032
Chet Jogia <chetjogia@gmail.com>
2023-01-09 16:40:26 +0000

    added info to cards and ghave them rendering on screen

--- home-grown/components/ListingCard/ListingCard.js
+++ home-grown/components/ListingCard/ListingCard.js
@@ -1,9 +1,20 @@
-function ListingCard({ description }) {
+
+
+function ListingCard({ profile_picture, rating, username, location, crop_type, plot_size, description }) {
   return (
     <div>
-      <h1>{description}</h1>
+      <img src={profile_picture} alt="profile"/>
+      <h3>Username: {username}</h3>
+      <p>User Rating: {rating}</p>
+      <p>Location:{location}</p>
+      <p>Crop:{crop_type}</p>
+      <p>Plot Size:{plot_size}</p>
+      <h1>Description: {description}</h1>
     </div>
   );
 }
 
 export default ListingCard;
+
+
+  //key data for cards = username, location, croptype, plotsize, description, user image, rating
--- home-grown/components/ListingContainer/ListingContainer.js
+++ home-grown/components/ListingContainer/ListingContainer.js
@@ -1,5 +1,6 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import ListingCard from "../ListingCard/ListingCard";
+import styles from "../../styles/ListingContainer.module.css";
 
 function ListingContainer() {
   //fetch data from api
@@ -8,6 +9,7 @@ function ListingContainer() {
   // add search bar
   //create dropdown to search by location and onClick
 
+  //key data for cards = username, location, croptype, plotsize, description, user image, rating
   useEffect(() => {
     getPostData();
   });
@@ -18,18 +20,24 @@ function ListingContainer() {
     const response = await fetch(
       "http://localhost:3000/api/staticdata_all_tables_joined"
     );
-   
+
     const data = await response.json();
-    
+
     setPosts(data);
-    console.log(data)
+    console.log(data);
   }
 
   if (!posts) {
     return <div>...Loading</div>;
   } else {
     return (
-     posts.map((element) => <div><ListingCard description={element.description} /></div>) 
+      <div className={styles["card-container"]}>
+        {posts.map((element) => (
+          <div className={styles.card}>
+            <ListingCard profile_picture={element.profile_picture} rating={element.rating} username={element.username} location={element.location} crop_type={element.crop_type} plot_size={element.plot_size} description={element.description} />
+          </div>
+        ))}
+      </div>
     );
   }
 }
