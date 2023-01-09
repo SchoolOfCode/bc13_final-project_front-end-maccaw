c5c905a321861ec8f8227ecb23062fc9c0d038fd
willbyrne <will.bl.byrne@gmail.com>
2023-01-09 12:15:13 +0000

    context in progress

--- home-grown/context/AuthContext.js
+++ home-grown/context/AuthContext.js
@@ -2,11 +2,14 @@ import React, { useContext, useEffect} from "react";
 import { Auth } from "firebase/auth";
 import { auth } from "../config/firebase";
 
+//creates authorisation context & stored in variable
 const AuthContext=React.createContext()
 
 export function useAuth()
+const {signUp, logIn, logOut} = useAuth
 
 export function AuthProvider() {
+    //store current user login details
     const [currentUser, setCurrentUser] = useState()
 
     function signUp(email, password) {
