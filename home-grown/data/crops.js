d09893ad05f3de20aa2c18733113b2db7889d4b8
MarwaDawood <marwa.dawood@hotmail.com>
2023-01-09 12:15:13 +0000

    crops

---
+++ home-grown/data/crops.js
@@ -0,0 +1,95 @@
+ [{
+      "crop_id": 1,
+      "crop_name": "White Potatoes",
+      "crop_image": "https://as2.ftcdn.net/v2/jpg/02/03/93/93/1000_F_203939367_W1OoDfEmPoHO18PYhFYtTRYyEAGCo5pA.jpg",
+      "crop_season": "Spring",
+      "crop_difficulty": 2,
+      "crop_harvest_time": "Summer",
+      "crop_type": "Tuber Vegetable"
+    },
+    {
+      "crop_id": 2,
+      "crop_name": "Carrots",
+      "crop_image": "https://as1.ftcdn.net/v2/jpg/02/99/52/10/1000_F_299521098_voPeHhesdOef4c6ial9GvrCjzKGyGZl1.jpg",
+      "crop_season": "Spring or Early summer",
+      "crop_difficulty": 2,
+      "crop_harvest_time": "Fall or Early Winter",
+      "crop_type": "Root Vegetable"
+    },
+    {
+      "crop_id": 3,
+      "crop_name": "White Mushrooms",
+      "crop_image": "https://cdn-icons-png.flaticon.com/512/1652/1652089.png",
+      "crop_season": "Spring or Fall",
+      "crop_difficulty": 4,
+      "crop_harvest_time": "Early Winter",
+      "crop_type": "Fungus"
+    },
+    {
+      "crop_id": 4,
+      "crop_name": "White Onions",
+      "crop_image": "https://cdn-icons-png.flaticon.com/512/1790/1790392.png",
+      "crop_season": "Spring",
+      "crop_difficulty": 2,
+      "crop_harvest_time": "Summer",
+      "crop_type": "Bulb Vegetable"
+    },
+    {
+      "crop_id": 5,
+      "crop_name": "Tomatoes",
+      "crop_image": "https://cdn-icons-png.flaticon.com/512/1790/1790387.png",
+      "crop_season": "Spring",
+      "crop_difficulty": 1,
+      "crop_harvest_time": "Summer",
+      "crop_type": "Fruits"
+    },
+    {
+      "crop_id": 6,
+      "crop_name": "Lettuce",
+      "crop_image": "https://cdn-icons-png.flaticon.com/512/424/424227.png",
+      "crop_season": "Spring or Fall",
+      "crop_difficulty": 2,
+      "crop_harvest_time": "Summer or Winter",
+      "crop_type": "Leafy Vegetable"
+    },
+    {
+      "crop_id": 7,
+      "crop_name": "Courgette",
+      "crop_image": "https://cdn-icons-png.flaticon.com/512/536/536509.png",
+      "crop_season": "Spring",
+      "crop_difficulty": 1,
+      "crop_harvest_time": "Summer or Early Fall",
+      "crop_type": "Squash Vegetable"
+  },
+  {
+    "crop_id": 8,
+    "crop_name": "Blackberries",
+    "crop_image": "https://cdn-icons-png.flaticon.com/512/3137/3137134.png",
+    "crop_season": "Spring or Fall",
+    "crop_difficulty": 2,
+    "crop_harvest_time": "Summer or Winter",
+    "crop_type": "Fruits"
+    
+  },
+    {
+    "crop_id": 9,
+    "crop_name": "Strawberries",
+    "crop_image": "https://cdn-icons-png.flaticon.com/512/590/590685.png",
+    "crop_season": "Spring",
+    "crop_difficulty": 2,
+    "crop_harvest_time": "Summer",
+    "crop_type": "Fruits"
+    },
+       {
+    "crop_id": 10,
+    "crop_name": "Raspberries",
+    "crop_image": "https://cdn-icons-png.flaticon.com/512/1515/1515032.png",
+    "crop_season": "Spring or Fall",
+    "crop_difficulty": 2,
+    "crop_harvest_time": "Summer or Winter",
+    "crop_type": "Fruits"
+       }
+    
+
+]
+
