77c2bd2e1ec9e5fefeb782290a8e747ca1013113
Julio Camargo <jcamargo@hotmail.co.uk>
2023-01-09 11:49:01 +0000

    plots js data input

---
+++ home-grown/data/plots.js
@@ -0,0 +1,54 @@
+ [{
+      "plot_id":"AbCdEfGhIjKlMnOpQrStUv",
+      "fireable_id":17,
+      "plot_size":30.00,
+      "percentage_used":"https://cdn.images.express.co.uk/img/dynamic/13/590x/616251_1.jpg",
+      "plot_image":"TS26 8JA"
+   },
+   {
+      "plot_id":"WxYz0123456789ABCDEFGH",
+      "fireable_id":35,
+      "plot_size":50.00,
+      "percentage_used":"https://i.redd.it/g2xcbacjg5s51.jpg",
+      "plot_image":"ME17 4FY"
+   },
+   {
+      "plot_id":"IJKLMNOPQRSTUVWXYZabcde",
+      "fireable_id":43,
+      "plot_size":30.00,
+      "percentage_used":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/veg-garden-1525260021.jpg",
+      "plot_image":"EX15 1YB"
+   },
+   {
+      "plot_id":"fghijklmnopqrstuvwxyz01",
+      "fireable_id":29,
+      "plot_size":70.00,
+      "percentage_used":"https://2.bp.blogspot.com/-TTzw-pQZWh4/Ud6kK84jasI/AAAAAAAAB4I/GgV6_Z-qjDI/s1600/backyard-raised-garden.jpg",
+      "plot_image":"DN36 5SW"
+   },
+   {
+      "plot_id":"23456789ABCDEFGHIJKLMNO",
+      "fireable_id":14,
+      "plot_size":40.00,
+      "percentage_used":"https://www.organiclea.org.uk/wp-content/uploads/clydeplace2-600x375.jpg",
+      "plot_image":"PL15 9HA"
+   },
+   {
+      "plot_id":"PQRSTUVWXYZabcdefghijkl",
+      "fireable_id":47,
+      "plot_size":40.00,
+      "percentage_used":"https://www.groundwork.org.uk/wp-content/uploads/2019/09/1900-x-1080-1-1.png",
+      "plot_image":"NR32 3LN"
+   },
+   {
+      "plot_id":"mnopqrstuvwxyz0123456789",
+      "fireable_id":31,
+      "plot_size":100.00,
+      "percentage_used":"https://www.food4families.org.uk/userfiles/images/SGA%20growallot.jpg",
+      "plot_image":"CB25 9BL"
+   },
+   {
+      "plot_id":"ABCDEFGHIJKLMNOPQRSTUVW",
+      "fireable_id":24,
+      "plot_size":90
+   }]
