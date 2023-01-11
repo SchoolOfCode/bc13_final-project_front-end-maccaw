9869e86bd68633059f1bc8b2f166d447b18fedda
willbyrne <will.bl.byrne@gmail.com>
2023-01-11 13:42:51 +0000

    carousel partially working

---
+++ home-grown/components/Dashboard/Carousel.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import dashboardStyles from "../../styles/DashboardContainer.module.css";
+
+export default class ImageCarousel extends Component {
+  render() {
+    return (
+      <div className={dashboardStyles.carouselContainer}>
+        <Carousel>
+          <div className={dashboardStyles.carouselImage}>
+            <img
+              src="https://images.immediate.co.uk/production/volatile/sites/10/2018/10/2048x1365-How-to-start-an-allotment-LI805369-c94303d.jpg"
+              alt="image1"
+            />
+            <p className="legend">Image 1</p>
+          </div>
+          <div className={dashboardStyles.carouselImage}>
+            <img src="https://i.redd.it/g2xcbacjg5s51.jpg" alt="image2" />
+            <p className="legend">Image 2</p>
+          </div>
+          <div className={dashboardStyles.carouselImage}>
+            <img
+              src="https://2.bp.blogspot.com/-TTzw-pQZWh4/Ud6kK84jasI/AAAAAAAAB4I/GgV6_Z-qjDI/s1600/backyard-raised-garden.jpg"
+              alt="image3"
+            />
+            <p className="legend">Image 3</p>
+          </div>
+        </Carousel>
+      </div>
+    );
+  }
+}
--- home-grown/pages/dashboard.js
+++ home-grown/pages/dashboard.js
@@ -2,37 +2,38 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../context/AuthContext";
 import { Title } from "../components/Dashboard/Title";
+import Carousel from "../components/Dashboard/Carousel";
 
 export default function Dashboard() {
   const [err, setErr] = useState("");
   const { currentUser, logout, isUserAuthenticated } = useAuth();
   const router = useRouter();
-  const [userData, setUserData] = useState()
-  const [isLoading, setIsLoading] = useState(true)
+  const [userData, setUserData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    getData()
-  },[]);
+    getData();
+  }, []);
 
+  async function getData() {
+    const response = await fetch(
+      "http://localhost:3000/api/staticdata_all_tables_joined"
+    );
+    const data = await response.json();
+    console.log("DATA", data);
+    console.log("CURRENT USER", currentUser);
 
-  async function getData(){
-    const response = await fetch( "http://localhost:3000/api/staticdata_all_tables_joined")
-    const data = await response.json()
-    console.log("DATA", data)
-    console.log("CURRENT USER",currentUser)
-
-    let user = data.filter(element => element.firebase_id === currentUser.uid)
-    setUserData(user)
-    setIsLoading(false)
+    let user = data.filter(
+      (element) => element.firebase_id === currentUser.uid
+    );
+    setUserData(user);
+    setIsLoading(false);
   }
-
 
   async function handleLogout() {
     setErr("");
 
     try {
-      console.log("Im here line 15");
       await logout();
-      console.log("Im here line 17");
       router.push("/login");
     } catch {
       setErr("failed to log out");
@@ -45,17 +46,15 @@ export default function Dashboard() {
   }
   console.log(currentUser, "here");
 
-  if(isLoading){
-    return <div>Loading...</div>
-  }
-  else{
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
     return (
       <div>
         <Title userData={userData} />
         {/* <button onClick={handleLogout}>Log Out</button> */}
-        
+        <Carousel showThumbs={false} />
       </div>
     );
   }
-
 }
