dd23a6d213d2cc010ea325019b4d426e1679ba0a
edmundsamina <edmundsamina@gmail.com>
2023-01-11 09:43:50 +0000

    home page auth removed

--- home-grown/pages/index.js
+++ home-grown/pages/index.js
@@ -4,14 +4,8 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const { currentUser } = useAuth();
-  if(!currentUser){
-    return <div>...Loading</div>
-  }
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <p>{currentUser.email}</p>
-    </div>
-  );
+      <div>
+        <h1>Hello World</h1>
+      </div>
+  
 }
