bff7322685dfb688aec679e8dc18927a16c3d592
edmundsamina <edmundsamina@gmail.com>
2023-01-12 15:50:10 +0000

    cypress testing navigating through different pages set up

--- home-grown/cypress/e2e/spec.cy.js
+++ home-grown/cypress/e2e/spec.cy.js
@@ -2,6 +2,8 @@ describe("visits the home page and then navigates to the listings page", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000");
     cy.get('[href="/listings"] > li').click().wait(1000);
+
+    cy.url().should("be.equal", "http://localhost:3000/listings");
     cy.get(".ListingContainer_input-bar__ZDahT")
       .type("strawberries")
       .get(".ListingContainer_search-button__IcPHz")
@@ -12,6 +14,8 @@ describe("visits the home page and then navigates to the listings page", () => {
 describe("visit listings page, searches by post-code and then clears search", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000/listings").wait(1000);
+    cy.url().should("be.equal", "http://localhost:3000/listings");
+
     cy.get(".ListingContainer_input-bar__ZDahT")
       .type("M")
       .wait(1000)
@@ -22,17 +26,32 @@ describe("visit listings page, searches by post-code and then clears search", ()
   });
 });
 
-describe.only("visit homepage, navigates to log-in, inputs details. Then clicks forgot password", () => {
+describe("visit homepage, navigates to log-in, inputs details. Then clicks forgot password", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000/").wait(1000);
     cy.get(":nth-child(4) > li").click().wait(1000);
+    cy.url().should("be.equal", "http://localhost:3000/login");
+
     cy.get(".Login_form-email-input__3q0BK > .Login_input__r_1vS").type(
       "amina@gmail.com"
     );
     cy.get(".Login_form-password-input__XOMuN > .Login_input__r_1vS").type(
       "passrd"
     );
-
     cy.get(".Login_button__ejJ5H").click().get("h2").should("be.visible");
+  });
+});
+
+describe("visit homepage and checks that the buttons navigate to the correct page", () => {
+  it("fails", () => {
+    cy.visit("http://localhost:3000/").wait(1000);
+    cy.get(".Home_button-two__xBO0p")
+      .click()
+      .url()
+      .should("be.equal", "http://localhost:3000/listings");
+    cy.get(".Home_button-one__8MDyp")
+      .click()
+      .url()
+      .should("be.equal", "http://localhost:3000/post");
   });
 });
