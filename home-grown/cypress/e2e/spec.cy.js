ca177c828f0b4e8b386de88d834c6d26d7478a4c
edmundsamina <edmundsamina@gmail.com>
2023-01-12 15:28:21 +0000

    test navigating to log in page -completed

--- home-grown/cypress/e2e/spec.cy.js
+++ home-grown/cypress/e2e/spec.cy.js
@@ -1,7 +1,38 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('[href="/listings"] > li').click()
-    cy.get('.ListingContainer_input-bar__ZDahT').type('strawberries').get('.ListingContainer_search-button__IcPHz').click()
-  })
-})
+describe("visits the home page and then navigates to the listings page", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[href="/listings"] > li').click().wait(1000);
+    cy.get(".ListingContainer_input-bar__ZDahT")
+      .type("strawberries")
+      .get(".ListingContainer_search-button__IcPHz")
+      .click();
+  });
+});
+
+describe("visit listings page, searches by post-code and then clears search", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000/listings").wait(1000);
+    cy.get(".ListingContainer_input-bar__ZDahT")
+      .type("M")
+      .wait(1000)
+      .get(".ListingContainer_search-button__IcPHz")
+      .click()
+      .wait(1000);
+    cy.get(".ListingContainer_clear-button__jewrU").click();
+  });
+});
+
+describe.only("visit homepage, navigates to log-in, inputs details. Then clicks forgot password", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000/").wait(1000);
+    cy.get(":nth-child(4) > li").click().wait(1000);
+    cy.get(".Login_form-email-input__3q0BK > .Login_input__r_1vS").type(
+      "amina@gmail.com"
+    );
+    cy.get(".Login_form-password-input__XOMuN > .Login_input__r_1vS").type(
+      "passrd"
+    );
+
+    cy.get(".Login_button__ejJ5H").click().get("h2").should("be.visible");
+  });
+});
