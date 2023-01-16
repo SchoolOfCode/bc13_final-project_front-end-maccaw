13169df612541ebdc256ff656542f2a5e86707f2
MarwaDawood <marwa.dawood@hotmail.com>
2023-01-16 11:03:51 +0000

    second push

--- home-grown/pages/posts.js
+++ home-grown/pages/posts.js
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/NewPost.module.css";
 import PostForm from "../components/PostForm/PostForm";
 import ListingCard from "../components/ListingCard/ListingCard";
 
+
+const [userPost, setUserPost] = useState("");
+
+
+async function getPostData() {
+    const response = await fetch(
+      "https://homegrown-backend.onrender.com/api/homegrown/posts"
+    );
+    const data = await response.json();
+    setPosts(data.payload);
+    console.log(data);
+  }
+
+
+
+
 export default function NewPost() {
+  useEffect(() => 
+{ 
+  getPostData() 
+}, [])
   return (
     <div>
       <PostForm></PostForm>
