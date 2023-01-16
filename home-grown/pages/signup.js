b4e04c5234814e6d5e137d756716328614ef0398
edmundsamina <edmundsamina@gmail.com>
2023-01-16 15:13:30 +0000

    register page front-end completed

--- home-grown/pages/signup.js
+++ home-grown/pages/signup.js
@@ -1,51 +1,201 @@
 import React, { useRef, useState } from "react";
-import { useAuth } from '../context/AuthContext.js'
+import { useAuth } from "../context/AuthContext.js";
 import { useRouter } from "next/router";
+import styles from "../styles/Signup.module.css";
+
+ //object of icon urls, so that the url can be saved in the database -> set icon to be animalIcons[e.target.name]
+const animalIcons = {
+  bunny: "https://cdn-icons-png.flaticon.com/512/4775/4775505.png",
+  duck: "https://cdn-icons-png.flaticon.com/512/1326/1326405.png",
+  cow: "https://cdn-icons-png.flaticon.com/512/2523/2523618.png",
+  sheep: "https://cdn-icons-png.flaticon.com/512/9308/9308986.png",
+  fox: "https://cdn-icons-png.flaticon.com/512/4322/4322992.png",
+  goat: "https://cdn-icons-png.flaticon.com/512/6525/6525852.png"
+};
 
 export default function SignUp() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
-  const { signUp } = useAuth()
-  const [err, setErr] = useState('')
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
+  const { signUp } = useAuth();
+  const [err, setErr] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [iconLabel, setIconLabel] = useState("SELECT ICON")
+  const [signUpForm, setSignUpFrom] = useState({
+    firstName: " ",
+    lastName: "",
+    email: "",
+    username: "",
+    icon: "",
+  });
 
- 
-  async function handleSubmit(e){
-    e.preventDefault()
+  const router = useRouter();
 
-    if(passwordRef.current.value !== passwordConfirmRef.current.value){
-      return setErr('Passwords do not match')
+  //function that conditionally shows the drop-down icons
+  function dropDown(){
+    setShow(!show)
+  }
+
+  //function that sets the selected icon to be in the form (as the image url) and then unshows the drop down
+  function iconSelect(e){
+    setSignUpFrom({
+      ...signUpForm,
+      icon: animalIcons[e.target.alt],
+    });
+    setIconLabel(e.target.alt)
+    setShow(false)
+  }
+
+  //function that sets the form data (apart from the profile icon)
+  function formData(e) {
+  
+    setSignUpFrom({
+      ...signUpForm,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+    //firebase registery of the user - submits the email and password to firbase
+  async function handleSubmit(e) {
+    console.log(signUpForm);
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setErr("Passwords do not match");
     }
 
     try {
-      setErr('')
-      setLoading(true)
-      await signUp(emailRef.current.value,passwordRef.current.value )
-      router.push("/dashboard")
+      setErr("");
+      setLoading(true);
+      await signUp(emailRef.current.value, passwordRef.current.value);
+      router.push("/dashboard");
     } catch {
-      setErr('Failed to create an account')
+      setErr("Failed to create an account");
     }
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      {err && <h2>{err}</h2>}
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input type="email" ref={emailRef} />
-
-        <label>Password</label>
-        <input type="password" ref={passwordRef} />
-
-        <label>Confirm your Password</label>
-        <input type="password" ref={passwordConfirmRef} />
-
-        <button disabled={loading} type="submit">Sign Up</button>
-      </form>
+    <div className={styles["log-in-container"]}>
+      <div className={styles["glass-container"]}>
+        <div className={styles["log-in-card"]}>
+          {err && <h2>{err}</h2>}
+          <div className={styles["form-container"]}>
+            <div className={styles["name-container"]}>
+              <div className={styles["first-name"]}>
+                <label className={styles["label"]}>Your First-Name:</label>
+                <input
+                  onChange={formData}
+                  name="firstName"
+                  className={styles["input"]}
+                  placeholder="First-Name"
+                  type="text"
+                />
+              </div>
+              <div className={styles["last-name"]}>
+                <label className={styles["label"]}>Your Last-Name:</label>
+                <input
+                  onChange={formData}
+                  name="lastName"
+                  className={styles["input"]}
+                  placeholder="Last-Name"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div className={styles["icon-username-container"]}>
+         
+              <div className={styles["dropdown"]}>
+              <label className={styles["label"]}>Profile Icon:</label>
+                <button onClick={dropDown} className={styles["dropbtn"]}>
+                {iconLabel.toUpperCase()}
+                </button>
+                {show && <div id="myDropdown" className={styles["dropdown-content"]}>
+                  <img
+                  onClick={iconSelect}
+                    id="bunny"
+                    src="https://cdn-icons-png.flaticon.com/512/4775/4775505.png"
+                    alt="bunny"
+                  />
+                  <img
+                  onClick={iconSelect}
+                    id="duck"
+                    src="https://cdn-icons-png.flaticon.com/512/1326/1326405.png"
+                    alt="duck"
+                  />
+                  <img
+                  onClick={iconSelect}
+                    id="cow"
+                    src="https://cdn-icons-png.flaticon.com/512/2523/2523618.png"
+                    alt="cow"
+                  />
+                  <img
+                  onClick={iconSelect}
+                    id="sheep"
+                    src="https://cdn-icons-png.flaticon.com/512/7923/7923884.png"
+                    alt="sheep"
+                  />
+                  <img
+                  onClick={iconSelect}
+                    id="fox"
+                    src="https://cdn-icons-png.flaticon.com/512/4322/4322992.png"
+                    alt="fox"
+                  />
+                  <img
+                  onClick={iconSelect}
+                    id="goat"
+                    src="https://cdn-icons-png.flaticon.com/512/6525/6525852.png"
+                    alt="goat"
+                  />
+                </div> }
+              </div>
+              <div className={styles["user-name"]}>
+                <label className={styles["label"]}>User-Name:</label>
+                <input
+                  onChange={formData}
+                  name="username"
+                  className={styles["input"]}
+                  placeholder="User-Name"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div className={styles["form-email-input"]}>
+              <label className={styles["label"]}>Your Email:</label>
+              <input
+                onChange={formData}
+                name="email"
+                className={styles["input"]}
+                placeholder="Email"
+                type="email"
+                ref={emailRef}
+              />
+            </div>
+            <div className={styles["form-password-input"]}>
+              <label className={styles["label"]}>Your Password:</label>
+              <input
+                className={styles["input"]}
+                placeholder="Password"
+                type="password"
+                ref={passwordRef}
+              />
+            </div>
+            <div className={styles["form-password-input"]}>
+              <label className={styles["label"]}>Confirm your Password</label>
+              <input
+                className={styles["input"]}
+                placeholder="Confirm Password"
+                type="password"
+                ref={passwordConfirmRef}
+              />
+            </div>
+            <button onClick={handleSubmit} className={styles.button} disabled={loading} type="submit">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
