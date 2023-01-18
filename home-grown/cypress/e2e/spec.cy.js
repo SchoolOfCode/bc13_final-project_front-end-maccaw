412a4c1376d60b2ca015a153d34b5d92cc881613
Julio Camargo <jcamargo@hotmail.co.uk>
2023-01-18 11:57:42 +0000

    cypress test 2

--- home-grown/cypress/e2e/spec.cy.js
+++ home-grown/cypress/e2e/spec.cy.js
@@ -7,7 +7,7 @@ describe("visits the home page and then navigates to the listings page", () => {
     cy.get(".ListingContainer_input-bar__ZDahT")
       .type("strawberries")
       .get(".ListingContainer_search-button__IcPHz",{ timeout: 10000 })
-      .click();
+      .click({timeout: 10000});
   });
 });
 
@@ -35,5 +35,20 @@ describe("visit homepage, navigates to log-in, inputs details. Then clicks forgo
       "passrd"
     );
     cy.get(".Login_button__ejJ5H").click().get("h2").should("be.visible");
+  });
+});
+
+describe("visit homepage, navigates to log-in, inputs details. and checke if logs in", () => {
+  it .only("passes", () =>{
+    // cy.session('login', ()=>{
+    cy.visit("http://localhost:3000/", {timeout: 10000});
+    cy.get("ul > :nth-child(4) > li").click({timeout: 10000});
+    cy.get(".Login_form-email-input__3q0BK > .Login_input__r_1vS").type(
+      "amina@gmail.com");
+    cy.get('.Login_form-password-input__XOMuN > .Login_input__r_1vS').type('password')
+    cy.get(".Login_button__ejJ5H").click({timeout: 10000});
+    cy.url().should("be.equal", "http://localhost:3000/dashboard"); 
+    cy.get('[href="/"] > li').click({timeout: 10000});
+    // });
   });
 });
