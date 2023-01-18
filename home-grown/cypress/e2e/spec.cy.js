8305791c2934b9ac93511a30c41783980f2bf128
Julio Camargo <jcamargo@hotmail.co.uk>
2023-01-18 10:42:00 +0000

    test pass

--- home-grown/cypress/e2e/spec.cy.js
+++ home-grown/cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe("visits the home page and then navigates to the listings page", () => {
     cy.visit("http://localhost:3000");
     cy.get('[href="/listings"] > li', { timeout: 10000 }).click();
 
-    cy.url().should("be.equal", "http://localhost:3000/listings");
+    cy.url({timeout: 10000}).should("be.equal", "http://localhost:3000/listings");
     cy.get(".ListingContainer_input-bar__ZDahT")
       .type("strawberries")
       .get(".ListingContainer_search-button__IcPHz",{ timeout: 10000 })
