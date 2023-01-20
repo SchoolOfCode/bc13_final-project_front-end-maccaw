93ac027197d3e82e6f90323b2071562966befac7
Julio Camargo <jcamargo@hotmail.co.uk>
2023-01-20 11:16:24 +0000

    initial set up page

--- home-grown/pages/dashboard.js
+++ home-grown/pages/dashboard.js
@@ -3,13 +3,11 @@ import { useRouter } from "next/router";
 import { useAuth } from "../context/AuthContext";
 import { Title } from "../components/Dashboard/Title";
 import Profile from "../components/Dashboard/Profile";
-import Image from 'next/image'
+import Image from "next/image";
 import PlotChart from "../components/Dashboard/PlotChart";
 import styles from "../styles/DashboardContainer.module.css";
 import CropTableContainer from "../components/Dashboard/CropTable/CropTableContainer";
-import plot from "../public/illustrations/plot.jpg"
-
-
+import plot from "../public/illustrations/plot.jpg";
 
 export default function Dashboard() {
   const [err, setErr] = useState("");
@@ -20,20 +18,16 @@ export default function Dashboard() {
   const [userPosts, setUserPosts] = useState();
 
   const newUserImages = {
-    plot_image: "https://t3.ftcdn.net/jpg/02/68/55/60/360_F_268556011_PlbhKss0alfFmzNuqXdE3L0OfkHQ1rHH.jpg"
-
-
-
-  }
+    plot_image:
+      "https://t3.ftcdn.net/jpg/02/68/55/60/360_F_268556011_PlbhKss0alfFmzNuqXdE3L0OfkHQ1rHH.jpg",
+  };
 
   if (!currentUser) {
     router.push("/login");
   }
 
   useEffect(() => {
-   
     if (currentUser) {
- 
       getData();
       getPosts();
     }
@@ -55,7 +49,6 @@ export default function Dashboard() {
     const data = await response.json();
 
     setUserPosts(data.payload);
-    
   }
 
   async function getData() {
@@ -71,7 +64,7 @@ export default function Dashboard() {
     );
 
     const data = await response.json();
-    console.log(data)
+    console.log(data);
     setUserData(data.payload[0]);
     setIsLoading(false);
   }
@@ -87,41 +80,40 @@ export default function Dashboard() {
     }
   }
 
-
-
   if (isLoading) {
     return <div>Loading...</div>;
   } else {
     return (
       <div className={styles["main-container"]}>
-
         <Title userData={userData} />
-       
 
-       <div className={styles["left-container"]}>
-         {userData.plot_image ? <Image src={plot} className={styles["plot-picture"]} /> :
-         <Image src={userData["plot_image"]} className={styles["plot-picture"]} />
-         }
-         <Profile className={styles["profile-container"]} userData={userData}/>
+        <div className={styles["left-container"]}>
+          {userData.plot_image ? (
+            <Image src={plot} className={styles["plot-picture"]} />
+          ) : (
+            <Image
+              src={userData["plot_image"]}
+              className={styles["plot-picture"]}
+            />
+          )}
+          <Profile
+            className={styles["profile-container"]}
+            userData={userData}
+          />
+        </div>
+        <div className={styles["right-container"]}>
+          <div className={styles["right-container-top"]}>
+            <PlotChart userPosts={userPosts} />
+          </div>
+
+          <div className={styles["right-container-bottom"]}>
+            <CropTableContainer
+              className={styles["crop-table-container"]}
+              userPosts={userPosts}
+            />
+          </div>
+        </div>
       </div>
-
-     
-
-      <div className={styles["right-container"]}>
-        <div className={styles["right-container-top"]}>
-           <PlotChart userPosts={userPosts}/>
-        </div>
-
-        <div className={styles["right-container-bottom"]}>
-           <CropTableContainer className={styles["crop-table-container"]} userPosts={userPosts}/>
-        </div>
-
-
-    </div>
-
-
-    </div>
-        
     );
   }
 }
