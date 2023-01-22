7138f1e67f9ac6615e0da95a289b61ee99700752
edmundsamina <edmundsamina@gmail.com>
2023-01-22 22:21:56 +0000

    landing page

--- home-grown/pages/index.js
+++ home-grown/pages/index.js
@@ -1,9 +1,6 @@
 import styles from "../styles/Home.module.css";
-import Link from 'next/link';
+import Link from "next/link";
 import { Carousel } from "../components/Landing/Carousel";
-
-
-
 
 export default function Home() {
   return (
@@ -13,32 +10,43 @@ export default function Home() {
           <h1 className={styles["title"]}> HOME GROWN</h1>
           <h3 className={styles["sub-title"]}>GOOD FOOD, GROWN TOGETHER</h3>
           <p className={styles.p}>
-            We are a community-driven platform that connects people online to
-            share their love of gardening and growing food. Our mission is to
-            make it easy for you to find gardening partners in your area and
-            make the most of your outdoor space. Whether you’re a seasoned green
-            thumb or just starting out, our platform has something for everyone.
-            Homegrown will help you find people to share seeds,
-            plants, and gardening tips with, as well as learn new skills and
-            techniques.{" "}
-           
+            Our platform is community-driven and focuses on connecting
+            individuals who have a passion for gardening and growing their own
+            food. Our goal is to simplify the process of finding gardening
+            partners in your local area and to help you fully utilize the
+            potential of your outdoor space through shared knowledge and
+            resources.{" "}
           </p>
           <div className={styles["button-container"]}>
-      <Link href="/post"> 
-      <button className={styles["button-one"]}> POST PLOT </button> 
-      </Link>
-      <Link href="/listings">
-      <button className={styles["button-two"]}>VIEW LISTINGS</button>    
-      </Link>
-      
+            <Link href="/post">
+              <button className={styles["button-one"]}> POST PLOT </button>
+            </Link>
+            <Link href="/listings">
+              <button className={styles["button-two"]}>VIEW LISTINGS</button>
+            </Link>
           </div>
         </div>
-         
+      </div>
+      <br></br>
+      <br></br>
+      <div className={styles["our-mission-container"]}>
+      <div className={styles["statement-container"]}>
+        <h3 className={styles["statement-heading"]}>We help you Connect with others in a shared Goal</h3>
+        <p className={styles["statement-paragraph"]}>Whether you’re a seasoned green,thumb or just starting out, our platform has something for everyone. Home-grown will help you find people to share seeds, plants, and gardening tips with, as well as learn new skills and techniques.</p>
+      </div>
+      <div className={styles["mission-container-bottom"]}>
+      <div className={styles["mission-container-image"]}>
+    
+      </div>
+      <div className={styles["mission-container-cards"]}>
+      
+        </div>
+  
+</div>
       </div>
       <div className={styles["cards-container"]}>
-         <Carousel/>
-         
-         </div>
+        <Carousel />
+      </div>
     </div>
   );
 }
