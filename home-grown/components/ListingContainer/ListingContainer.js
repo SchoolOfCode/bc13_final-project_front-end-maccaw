c1385e7eff32845009c1059f579ff4fbcd89bdbd
willbyrne <will.bl.byrne@gmail.com>
2023-01-23 16:06:12 +0000

    search clearing on clear button click

--- home-grown/components/ListingContainer/ListingContainer.js
+++ home-grown/components/ListingContainer/ListingContainer.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import ListingCard from "../ListingCard/ListingCard";
 import styles from "../../styles/ListingContainer.module.css";
 
-function ListingContainer({search, posts}) {
+function ListingContainer({ search, posts }) {
   //fetch data from api
   //save the data in a state
   //map each entry of the array (in state) to a card
@@ -11,14 +11,23 @@ function ListingContainer({search, posts}) {
 
   //key data for cards = username, location, croptype, plotsize, description, user image, rating
 
-
   if (!posts) {
     return <div>...Loading</div>;
   } else {
     return (
       <div className={styles["card-container"]}>
         {posts.map((element) => (
-            <ListingCard user_email = {element.email} profile_picture={element.profile_picture} title={element.title} rating={element.rating} username={element.username} location={element.location} crop_name={element.crop_name} description={element.description} />
+          <ListingCard
+            key={element.posts_id}
+            user_email={element.email}
+            profile_picture={element.profile_picture}
+            title={element.title}
+            rating={element.rating}
+            username={element.username}
+            location={element.location}
+            crop_name={element.crop_name}
+            description={element.description}
+          />
         ))}
       </div>
     );
