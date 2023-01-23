40ec7d624bcb28eaf2496851ac8ddb43f937513a
Chet Jogia <chetjogia@gmail.com>
2023-01-23 16:24:48 +0000

    nav bar working

--- home-grown/components/NavBar/NavBar.js
+++ home-grown/components/NavBar/NavBar.js
@@ -3,57 +3,104 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import styles from "../../styles/Navbar.module.css";
+import { useAuth } from "../../context/AuthContext";
+import logo from "../../public/icons/logo.png"
+
 
 export default function NavBar() {
   const router = useRouter();
-
+  const { currentUser, logout } = useAuth();
   function reloadPage() {
     router.push("/");
   }
 
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false);
 
-  function burgerHandler(){
-    setActive(!active)
+  function burgerHandler() {
+    setActive(!active);
   }
 
-  return (
-    <nav className="navbar">
-      <div className="logo">
-     
-      </div>
-      <ul className={active ? "list-container active"  : "list-container notactive"}>
-        <Link href="/" className="text">
-          <li>Home</li>
-        </Link>
-        <Link href="/listings" className="text" >
-          <li>Listings</li>
-        </Link>
-        <Link href="/signup" className="text">
-          <li>Register</li>
-        </Link>
-        <Link href="/login" className="text">
-          <li>Log In</li>
-        </Link>
-        <Link href="/login" className="text">
-          <li>Post</li>
-        </Link>
-        <Link href="/dashboard" className="text">
-          <li>Dashboard</li>
-        </Link>
-      </ul>
 
-      <div onClick={burgerHandler} className={active ? "hamburger active":"hamburger notactive"}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
+  function logOut(){
+    setActive(!active);
+    logout()
+  }
 
+  if (currentUser) {
+    return (
+      <nav className="navbar">
+        <img className="logo" src="icons/logo.png"/>
+        <ul
+          className={
+            active ? "list-container active" : "list-container notactive"
+          }
+        >
+          <Link href="/" className="text" onClick={burgerHandler}>
+            <li>Home</li>
+          </Link>
+          <Link href="/listings" className="text" onClick={burgerHandler}>
+            <li>Listings</li>
+          </Link>
 
-    </nav>
+          <Link href="/login" className="text" onClick={burgerHandler}>
+            <li>Log Out</li>
+          </Link>
+          <Link href="/post" className="text" onClick={burgerHandler}>
+            <li>Post</li>
+          </Link>
+          <Link href="/dashboard" className="text" onClick={burgerHandler}>
+            <li>Dashboard</li>
+          </Link>
+        </ul>
 
+        <div
+          onClick={burgerHandler}
+          className={active ? "hamburger active" : "hamburger notactive"}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="navbar">
+        <div className="logo"></div>
+        <ul
+          className={
+            active ? "list-container active" : "list-container notactive"
+          }
+        >
+          <Link href="/" className="text" onClick={burgerHandler}>
+            <li>Home</li>
+          </Link>
+          <Link href="/listings" className="text" onClick={burgerHandler}>
+            <li>Listings</li>
+          </Link>
+          <Link href="/signup" className="text" onClick={burgerHandler}>
+            <li>Register</li>
+          </Link>
+          <Link href="/login" className="text" onClick={burgerHandler}>
+            <li>Log In</li>
+          </Link>
+          <Link href="/login" className="text" onClick={burgerHandler}>
+            <li>Post</li>
+          </Link>
+          <Link href="/dashboard" className="text" onClick={burgerHandler}>
+            <li>Dashboard</li>
+          </Link>
+        </ul>
 
-
-
-  );
+        <div
+          onClick={burgerHandler}
+          className={active ? "hamburger active" : "hamburger notactive"}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
+    );
+  }
 }
