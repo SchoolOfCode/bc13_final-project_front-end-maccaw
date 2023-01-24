8674a25188cc5b830349be0d7d4845225ecd993a
Julio Camargo <jcamargo@hotmail.co.uk>
2023-01-24 12:23:24 +0000

    set up

--- home-grown/components/Footer/Footer.js
+++ home-grown/components/Footer/Footer.js
@@ -2,12 +2,30 @@
 export default function Footer() {
     return (
         <footer>
-        <div className="footer">
-        <img src="/icons/facebook.png"/>
-        <img src="/icons/insta.png"/>
-        <img src = "icons/linkdin.png"/>
-        <img src = "icons/twitter.png"/>
-     
+        <p>Follow us on Linkedin:</p>
+
+        <div className="team-member">
+        <a className="icon-link" href="http://www.linkedin.com/in/alex-chappell-developer/">Alex</a>
+        </div>
+
+        <div className="team-member">
+        <a className="icon-link" href="http://www.linkedin.com/in/amina-edmunds"><p>Amina</p></a>
+        </div>
+
+        <div className="team-member">
+        <a className="icon-link" href="http://www.linkedin.com/in/chetanjogia/"><p>Chetan</p></a>
+        </div>
+
+        <div className="team-member">
+        <a className="icon-link" href="http://www.linkedin.com/in/julio-camargo-developer/"><p>Julio</p></a>
+        </div>
+
+        <div className="team-member">
+        <a className="icon-link" href="http://www.linkedin.com/in/marwa-dawood/"><p>Marwa</p></a>
+        </div>
+
+        <div className="team-member">
+        <a className="icon-link" href="http://www.linkedin.com/in/will-byrne-developer"><p>Will</p></a>
         </div>
         </footer>
     )
