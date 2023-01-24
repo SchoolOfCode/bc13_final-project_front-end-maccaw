305a18c6720c2c83742cb58b78ecf2218cc58dc4
willbyrne <will.bl.byrne@gmail.com>
2023-01-24 16:37:38 +0000

    removed comments

--- home-grown/components/ListingContainer/ListingContainer.js
+++ home-grown/components/ListingContainer/ListingContainer.js
@@ -3,14 +3,6 @@ import ListingCard from "../ListingCard/ListingCard";
 import styles from "../../styles/ListingContainer.module.css";
 
 function ListingContainer({ search, posts, showEmailToast }) {
-  //fetch data from api
-  //save the data in a state
-  //map each entry of the array (in state) to a card
-  // add search ba
-  //create dropdown to search by location and onClick
-
-  //key data for cards = username, location, croptype, plotsize, description, user image, rating
-
   if (!posts) {
     return <div>...Loading</div>;
   } else {
